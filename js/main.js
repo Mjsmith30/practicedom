9dccf848c2e77c5a01b9383c9d9d303490020345
Marques Smith <marquesjs21@yahoo.com>
2020-09-14 21:22:52 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let titleEl = document.getElementById('title');
+titleEl.style.textAlign = 'center';
+let pEl = document.querySelector('.cool');
+pEl.innerHTML = 'Comments for <strong> Today</strong>'
+pEl.style.color ='purple';
+
+console.log(pEl);
+let aEl = document.querySelector('a');
+
+aEl.setAttribute('href', 'https:www.google.com');
+
